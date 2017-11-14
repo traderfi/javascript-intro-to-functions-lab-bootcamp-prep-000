da8659233d0c6ecc0a57c0b4af33652e0573bb7e
Jay G <playerjayho@gmail.com>
2017-11-14 20:19:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string.toLowerCase){
+  if(string.toLowerCase == string){
     return 'I can\'t hear you!'
      }
   if(string === "I love you, Grandma."){
