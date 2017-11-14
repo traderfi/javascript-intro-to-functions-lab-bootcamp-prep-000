635d02825dfb949b61945ebbf0a23eb1e2c0ec65
Jay G <playerjayho@gmail.com>
2017-11-14 20:15:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
   if(string == "I love you, Grandma."){
     return 'I love you, too.'
      }
-  if(string.toUpperCase){
+  if(string == string.toUpperCase()){
     return 'YES INDEED!'
   }
 }
