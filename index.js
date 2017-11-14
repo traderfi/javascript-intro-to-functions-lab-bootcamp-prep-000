aa2ff25823e8705501ee5bffba3754ac5d028ed9
Jay G <playerjayho@gmail.com>
2017-11-14 20:18:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if(string.toLowerCase){
     return 'I can\'t hear you!'
      }
-  if(string == "I love you, Grandma."){
+  if(string === "I love you, Grandma."){
     return 'I love you, too.'
      }
   if(string == string.toUpperCase()){
